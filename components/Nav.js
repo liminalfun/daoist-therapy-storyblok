6008ca22df357b7d824bb3e0ece5919f0137f0c8
liminalfun <xelanawotka@gmail.com>
2021-08-16 22:48:09 -0400

    [SETUP]

---
+++ components/Nav.js
@@ -0,0 +1,82 @@
+import React from "react";
+import { ReactNode } from "react";
+import {
+	Box,
+	Flex,
+	Avatar,
+	HStack,
+	Link,
+	IconButton,
+	Button,
+	Menu,
+	MenuButton,
+	MenuList,
+	MenuItem,
+	MenuDivider,
+	useDisclosure,
+	useColorModeValue,
+	Stack,
+} from "@chakra-ui/react";
+import { HamburgerIcon, CloseIcon } from "@chakra-ui/icons";
+
+const Links = ["Clinic", "Instruction", "Online Courses", "About", "Contact"];
+
+const NavLink = ({ children }) => (
+	<Link
+		px={2}
+		py={1}
+		rounded={"md"}
+		_hover={{
+			textDecoration: "none",
+			bg: useColorModeValue("gray.200", "gray.700"),
+		}}
+		href={"#"}>
+		{children}
+	</Link>
+);
+
+export default function Nav() {
+	const { isOpen, onOpen, onClose } = useDisclosure();
+
+	return (
+		<nav>
+			<Box bg={useColorModeValue("gray.100", "gray.900")} px={4}>
+				<Flex
+					h={16}
+					alignItems={"center"}
+					justifyContent={"space-between"}>
+					<HStack spacing={8} alignItems={"center"}>
+						<Box>Daoist Therapy</Box>
+						<HStack
+							as={"nav"}
+							spacing={4}
+							display={{ base: "none", md: "flex" }}>
+							{Links.map((link) => (
+								<NavLink key={link}>{link}</NavLink>
+							))}
+						</HStack>
+					</HStack>
+					<IconButton
+						size={"md"}
+						icon={isOpen ? <CloseIcon /> : <HamburgerIcon />}
+						aria-label={"Open Menu"}
+						display={{ md: "none" }}
+						onClick={isOpen ? onClose : onOpen}
+					/>
+				</Flex>
+
+				{isOpen ? (
+					<Box pb={4} display={{ md: "none" }}>
+						<Stack as={"nav"} spacing={4}>
+							{Links.map((link) => (
+								<NavLink href={link} key={link}>
+									{link}
+								</NavLink>
+							))}
+						</Stack>
+					</Box>
+				) : null}
+			</Box>
+		</nav>
+	);
+}
