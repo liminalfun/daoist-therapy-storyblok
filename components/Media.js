248ee3bc8d9cf859fc7ed017529a57532f5d24c5
liminalfun <xelanawotka@gmail.com>
2021-09-04 13:29:50 -0400

    [FIX] media issue

--- components/Media.js
+++ components/Media.js
@@ -1,18 +1,12 @@
 import React from 'react'
 import Image from 'next/image'
 import { Box } from '@chakra-ui/react'
+
+
  
 const Media = ({ blok }) => (
   <Box className="">
-    <figure>
-      <Image 
-        src={blok.media.filename}
-        alt={blok.media.alt}
-        width={500}
-        height={500}
-      />
-      <figcaption>{blok.caption}</figcaption>
-    </figure>
+    <img src={blok.media.filename} alt={blok.media.alt}/>
   </Box>
 )
  
